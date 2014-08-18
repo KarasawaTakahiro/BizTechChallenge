b68a951c67a73ba5f8e435292c44b981e5f97c32
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2014-08-18 10:46:38 -0100

    new:    index.css

--- fuel/app/views/biztechpre/index.php
+++ fuel/app/views/biztechpre/index.php
@@ -1,27 +1,24 @@
-<?php 
-use \Model\Biztechpre;
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="utf-8">
+        <?php echo Asset::css('index.css'); ?>
         <title><?php echo $title; ?></title>
     </head>
     <body>
-        <h1><?php echo $title; ?></h1>
-
-        <?php echo Html::anchor('bbs/1', 'ENTER'); ?>
-
-        <?php echo Form::open(array('action'=>'biztechpre/login', 'id'=>'comment_form')); ?>
+        <h1 id="title"><?php echo $title; ?></h1>
+        <?php 
+        echo Html::anchor('bbs/1', 'ENTER', array('id'=>'anchor_enter'));
+        echo Form::open(array('action'=>'biztechpre/login', 'id'=>'comment_form')); 
+        ?>
             <table>
                 <tr>
-                    <td>Name:</td>
+                    <td id="str_name">Name:</td>
                     <td><input type="text" name="name" id="input_name"></td>
                     <td><div id="alert_name"></div></td>
                 </tr>
                 <tr>
-                    <td>Mail:</td>
+                    <td id="str_mail">Mail:</td>
                     <td><input type="text" name="mail" id="input_mail"></td>
                     <td><div id="alert_mail"></div></td>
                 </tr>
